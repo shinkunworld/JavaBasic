91f9e9fecd68e6489e49cfd8a5a84715428fee21
shinkunworld <shinkunworld@gmail.com>
2019-03-27 16:20:42 +0900

    상속,오버라이드

--- src/Static/Main.java
+++ src/Static/Main.java
@@ -2,6 +2,7 @@ package Static;
 
 public class Main {
 public static void main(String[] args) {
+	//static은 객체를 사용하지 않고 클래스에서 직접 공유가 가능하지만 많이쓰면 메모리에 과부하가 걸린다.
 		
 		FirstChild firstChild = new FirstChild();
 		firstChild.takeMoney(100);
---
+++ src/extend/Main.java
@@ -0,0 +1,18 @@
+package extend;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		
+		ChildMenu childMenu = new ChildMenu();
+		childMenu.makeChungGukJang();
+		childMenu.makeDoenJangGuk();
+		childMenu.makeGalbiJjim();
+		childMenu.makeHotDoenJangGuk();
+		childMenu.makeKongNaMool();
+		childMenu.MakeSoybean();
+		
+	}
+}
+
