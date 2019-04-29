e1d4cc88c2c6f56afda95ea1ac17b46195e0c442
shinkunworld <shinkunworld@gmail.com>
2019-04-29 17:00:12 +0900

    컴포넌트

---
+++ src/awtComponent/MainClass.java
@@ -0,0 +1,14 @@
+package awtComponent;
+
+import java.awt.Dimension;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		MakeFrame makeFrame = new MakeFrame();
+		makeFrame.setSize(new Dimension(500, 500));
+		makeFrame.setVisible(true);
+
+	}
+
+}
---
+++ src/swingComponent/MainClass.java
@@ -0,0 +1,12 @@
+package swingComponent;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		MakeFrame makeComp = new MakeFrame();
+		makeComp.pack();
+		makeComp.setVisible(true);
+
+	}
+
+}
